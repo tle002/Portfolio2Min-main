66333b5b66fd0541fa978a0d3b7f1cdf01141a1e
Abhishek Kumar Gupta <abhishekkumargupta7860@gmail.com>
2025-01-15 00:03:31 +0530

    all done

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -13,8 +13,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        // file has been uploaded successfull
-        // console.log("file is uploaded on cloudinary ", response.url);
+        //file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response.url;
 
